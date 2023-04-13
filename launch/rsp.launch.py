2a7c3f80667f5aee9be94ee13ef2ef897f6be95d
alexheijnen1980 <alex_heijnen@hotmail.com>
2023-04-13 15:37:35 +0000

    first attempt at robotic arm

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,14 +1,10 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
-
 
 def generate_launch_description():
 
@@ -29,13 +25,11 @@ def generate_launch_description():
         parameters=[params]
     )
 
-
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-
         node_robot_state_publisher
     ])
